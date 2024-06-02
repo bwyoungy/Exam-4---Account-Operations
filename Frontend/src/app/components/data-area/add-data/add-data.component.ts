05cdd6f2eb3fe021bada9e9b6d9a729ce865a8df
bwyoungy <youngyaacov@gmail.com>
2024-06-02 18:36:51 +0300

    add page to frontend to add new account operation

--- Frontend/src/app/components/data-area/add-data/add-data.component.ts
+++ Frontend/src/app/components/data-area/add-data/add-data.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DataService } from '../../../services/data.service';
+import { Router } from '@angular/router';
+import { AccountOperationModel } from '../../../models/accountoperation.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-data',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-data.component.html',
   styleUrl: './add-data.component.css'
 })
 export class AddDataComponent {
+  public operation = new AccountOperationModel();
+  public types = ["deposit", "withdrawal", "loan"];
+
+  public constructor(private dataService:DataService, private router:Router) {}
+
+  public async send():Promise<void> {
+    try {
+      this.operation.date = new Date();
+      console.log(this.operation);
+      
+      await this.dataService.addAccountOperation(this.operation);
+      alert("Account Operation has been added");
+      this.router.navigateByUrl("/home");
+    } catch (error:any) {
+      alert(error.message);
+    }
+  }
 
 }
