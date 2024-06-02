715b9c416924ef4095418a02f8e75287765eb398
bwyoungy <youngyaacov@gmail.com>
2024-06-02 18:33:52 +0300

    add data display per account number to frontend

--- Frontend/src/app/components/data-area/data-list/data-list.component.ts
+++ Frontend/src/app/components/data-area/data-list/data-list.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { AccountOperationModel } from '../../../models/accountoperation.model';
+import { DataService } from '../../../services/data.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-data-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './data-list.component.html',
   styleUrl: './data-list.component.css'
 })
 export class DataListComponent {
+  public accountNumber: number;
+  public operations: AccountOperationModel[];
 
+  public constructor(private dataService:DataService) {}
+
+  public async display() {
+    try {
+      if(!this.accountNumber) {
+        this.operations = null;
+        return;
+      }
+      this.operations = await this.dataService.getAccountOperations(this.accountNumber);
+    } catch (error:any) {
+      alert(error.message);
+    }
+  }
 }
