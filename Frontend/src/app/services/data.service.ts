4194f7182b0463b5e752ca78798e2a0477784026
bwyoungy <youngyaacov@gmail.com>
2024-06-02 18:31:43 +0300

    create dataservice in frontend to work on account operations

--- Frontend/src/app/services/data.service.ts
+++ Frontend/src/app/services/data.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AccountOperationModel } from '../models/accountoperation.model';
+import { appConfig } from '../app.config';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -8,7 +11,16 @@ export class DataService {
 
     constructor(private http: HttpClient) { }
 
-    public async getAll____() {
-        
+    // Get all operations of account
+    public async getAccountOperations(accountNumber:number):Promise<AccountOperationModel[]> {
+        const observable = this.http.get<AccountOperationModel[]>(appConfig.operationsUrl + accountNumber);
+        const operations = await firstValueFrom(observable);
+        return operations;
+    }
+
+    // Add account operation
+    public async addAccountOperation(operation:AccountOperationModel):Promise<void> {
+        const observable = this.http.post<AccountOperationModel>(appConfig.operationsUrl, operation);
+        const addedOperation = await firstValueFrom(observable);
     }
 }
