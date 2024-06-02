b42cab539d13788f869b0701c196735a30041c93
bwyoungy <youngyaacov@gmail.com>
2024-06-02 18:15:15 +0300

    add REST API paths to data controller for AccountOperations

--- Backend/src/controllers/data-controller.ts
+++ Backend/src/controllers/data-controller.ts
@@ -1,4 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
+import { accountOperationsLogic } from "../logic/accountoperations-logic";
+import { AccountOperationModel } from "../models/accountoperation-model";
 
 // Data controller:
 class DataController {
@@ -13,13 +15,25 @@ class DataController {
 
     // Register routes:
     private registerRoutes(): void {
-        this.router.get("/", this.getAll___);
+        this.router.get("/accountOperations/:accountNumber", this.getAccountOperations);
+        this.router.post("/accountOperations", this.addAccountOperation);
     }
 
-    // GET http://localhost:4000/api/___
-    private async getAll___(request: Request, response: Response, next: NextFunction): Promise<void> {
+    // GET http://localhost:4000/api/accountOperations/:accountNumber
+    private async getAccountOperations(request: Request, response: Response, next: NextFunction): Promise<void> {
         try {
-            // ...
+            const operations = await accountOperationsLogic.getAccountOperations(+request.params.accountNumber);
+            response.json(operations);
+        }
+        catch (err: any) { next(err); }
+    }
+
+    // POST http://localhost:4000/api/accountOperations
+    private async addAccountOperation(request: Request, response: Response, next: NextFunction): Promise<void> {
+        try {
+            const operation = new AccountOperationModel(request.body);
+            const addedOperation = await accountOperationsLogic.addAccountOperation(operation);
+            response.json(addedOperation);
         }
         catch (err: any) { next(err); }
     }
